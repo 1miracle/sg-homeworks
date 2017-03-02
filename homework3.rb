49759ddf74477f26c86e5da6126b607cf4cf6835
1miracle <marcus.trigger@mail.ru>
2017-03-02 11:53:23 +0200

    Add last task of second homework

---
+++ homework3.rb
@@ -0,0 +1,30 @@
+class HomeTask3
+  def initialize(max)
+    @max = max
+  end
+
+  def prime_array
+    @arr = []
+    (1..@max).each do |i|
+      @arr << i if is_prime?(i)
+    end
+    output
+  end
+
+  def is_prime?(num)
+    return false if num <= 1
+    Math.sqrt(num).to_i.downto(2).each { |i| return false if (num % i).zero? }
+    true
+  end
+
+  def output
+    p @arr
+    (@arr.size - 1).times do
+      p @arr.push(@arr.shift)
+    end
+  end
+end
+
+number = gets.to_i
+hello = HomeTask3.new(number)
+hello.prime_array
